02b2bbb8bda4cef58bc653aa8cd97fc7cce8b9b6
estefania.s <nia.soldati@gmail.com>
2020-08-19 19:27:20 -0300

    initial files upload

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+var usuario = prompt("Ingrese su nombre");
+var saludo = "Bienvenido/a al sitio"+ " " + usuario;
+console.log(saludo)
+
+var numero = parseInt(prompt("Por favor ingrese un número"));
+var numGuardado = 5;
+var suma = numero + numGuardado;
+console.log("El resultado es" + " " + suma);
+
+var nombre = prompt("Escriba su nombre");
+var pais = prompt("Escriba su pais de residencia"); 
+var bienvenida = "Bienvenida" + " " + nombre + " " + "de" + " " + pais;
+alert(bienvenida);
+
