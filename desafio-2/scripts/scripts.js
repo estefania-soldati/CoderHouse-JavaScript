318ac97a23541c7850f394b757e946bc3c2ae39e
estefania.s <nia.soldati@gmail.com>
2020-08-22 15:24:39 -0300

    desafio 2

---
+++ desafio-2/scripts/scripts.js
@@ -0,0 +1,15 @@
+var numero = prompt("Ingrese un número");
+if (numero > 1000) {
+  alert("El número ingresado es mayor a 1000");
+}
+
+
+var texto = prompt("Ingrese un saludo");
+if (texto == "Hola") {
+  console.warn("El saludo fue Hola");
+}
+
+var numeroDos = prompt("Ingrese otro número");
+if (numeroDos >= 10 && numeroDos <= 50) {
+  alert("El número esta entre el 10 y el 50");
+}
