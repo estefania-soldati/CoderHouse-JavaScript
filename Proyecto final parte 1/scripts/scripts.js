8c99bb30448a58893714a08c6a1bdb6e90695b49
estefania.s <nia.soldati@gmail.com>
2020-09-09 22:23:17 -0300

    proyecto final parte 1

---
+++ Proyecto final parte 1/scripts/scripts.js
@@ -0,0 +1,37 @@
+function mostrarMensajeDeBienvenida (){
+  var nombre = prompt("Por favor ingrese su nombre");
+  alert ("Bienvenido/a " + nombre);
+}
+
+mostrarMensajeDeBienvenida();
+
+function Auto (marca, modelo, anio, puertas){
+  this.marca = marca;
+  this.modelo = modelo;
+  this.anio = anio;
+  this.puertas = puertas;
+}
+
+function Duenio (nombre, nacimiento, pais, ciudad, telefono, mail){
+  this.nombre = nombre;
+  this.nacimiento = nacimiento;
+  this.pais= pais;
+  this.ciudad = ciudad;
+  this.telefono = telefono;
+  this.mail = mail;
+}
+
+var duenio1 = new Duenio("Lucas Peralta", "23/09/1990" , "Argentina", "Buenos Aires", "4587895", "luqui@gmail.com");
+
+function mostrarMensajeDeAgradecimiento() {
+  alert("Gracias " + duenio1.nombre + " Pronto nos comunicaremos con usted");
+}
+
+var autos = [];
+
+var auto1 = new Auto("citroen", "c4", 2018, 4); 
+var auto2 = new Auto("ford", "fiesta", 2010, 2); 
+
+autos.push(auto1,auto2);
+console.log(autos);
+
